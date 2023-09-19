a6cc145ba1f1e896f580b157fb7398bd8c0075e3
Unkookerinho <Unkookerinho@gmail.com>
2023-09-19 18:15:30 +0200

    Fixed homepage load animation
    It's positioned properly now

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const text = document.querySelector('.main__text');
   const button = document.querySelector('.main__button');
 
-  text.style.transform = 'translateX(5rem)';
+  text.style.transform = 'translateX(0)';
   text.style.opacity = '1';
-  button.style.transform = 'translateX(-5rem)';
+  button.style.transform = 'translateX(0)';
   button.style.opacity = '1';
 });
