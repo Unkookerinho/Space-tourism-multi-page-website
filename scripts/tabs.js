8f316e661bceb9989d215d3e284e475b4a897b68
Unkookerinho <Unkookerinho@gmail.com>
2023-09-21 15:00:10 +0200

    Optimized tabs functionality
    Started using data-target on all interactive elements.

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,19 +1,10 @@
 const options = document.querySelectorAll('#option');
 const interactive = document.querySelectorAll('.interactive');
-const interactiveImage = document.querySelectorAll('.interactive-image');
 
 // Animation on load
 document.addEventListener('DOMContentLoaded', () => {
   // Text animation
   interactive.forEach((element) => {
-    if (element.classList.contains('active')) {
-      setTimeout(() => {
-        element.style.opacity = 1;
-      }, 10);
-    }
-  });
-  // Image animation
-  interactiveImage.forEach((element) => {
     if (element.classList.contains('active')) {
       setTimeout(() => {
         element.style.opacity = 1;
@@ -26,39 +17,27 @@ document.addEventListener('DOMContentLoaded', () => {
 options.forEach((option) => {
   option.addEventListener('click', (event) => {
     event.preventDefault();
+    // Getting element's target
+    const targetId = option.getAttribute('data-target');
+
     // Execute only if element is not already active
     if (!option.classList.contains('active')) {
       // Deactivating other options
       options.forEach((otherOption) => {
         otherOption.classList.remove('active');
       });
-      // Text animation
       interactive.forEach((element) => {
-        element.style.opacity = 0;
-        element.classList.remove('active');
-      });
-      // Image animation
-      interactiveImage.forEach((element) => {
         element.style.opacity = 0;
         element.classList.remove('active');
       });
 
       // Activating clicked one
       option.classList.add('active');
-      // Text animation
-      interactive.forEach((element) => {
-        if (element.classList.contains(option.innerText.toLowerCase()))
-          element.classList.add('active');
 
-        setTimeout(() => {
-          element.style.opacity = 1;
-        }, 10);
-      });
-      // Image animation
-      interactiveImage.forEach((element) => {
-        if (element.classList.contains(option.innerText.toLowerCase()))
-          element.classList.add('active');
-
+      // Animate clicked one
+      const targetedElement = document.querySelectorAll(`#${targetId}`);
+      targetedElement.forEach((element) => {
+        element.classList.add('active');
         setTimeout(() => {
           element.style.opacity = 1;
         }, 10);
